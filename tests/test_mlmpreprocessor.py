9b5e547baaa47ff509f29325d8d8d8d802532307
jamesmf <jamesmf@fastmail.net>
2021-01-21 23:12:49 -0500

    freeze/unfreeze pretrainer

--- tests/test_mlmpreprocessor.py
+++ tests/test_mlmpreprocessor.py
@@ -1,7 +1,13 @@
 import pytest
 from cclm.preprocessing import MLMPreprocessor
 
-CORPUS = ["hello i am a test string", "hi there I am also a test string"]
+CORPUS = [
+    "hello i am a test string",
+    "hi there I am also a test string",
+    "this is more words and the length is longer",
+    "here is another for us to test the",
+    "vocabulary and in order for ther eto be enough sampled values for the tensorflow log uniform candidate sampler",
+]
 
 
 def test_prep_encode_string():
